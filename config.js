08bc83af51ffff7feb4a7d1d126dd81c05f5c7ca
Levi Bernard <levibernard@Levis-MacBook-Pro.local>
2018-07-23 22:00:10 -0700

    updated DATABASE_URL to point to mLab hosted db

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://user:password1@ds137687.mlab.com:37687/blog-post-seed-data-db';
 exports.PORT = process.env.PORT || 8080;
