6a7e501dda42df8a2860497ac171beba4cd73f12
Levi Bernard <levibernard@Levis-MacBook-Pro.local>
2018-07-25 20:03:05 -0700

    added rest of aurthors post endpoint details

--- server.js
+++ server.js
@@ -41,6 +41,39 @@ app.post('/authors', (req,res) => {
     }
   });
   
+  Author
+  .findOne({ userName: req.body.userName })
+  .then(author => {
+    if (author) {
+      const message = `Username already taken`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+    else {
+      Author
+        .create({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          userName: req.body.userName
+        })
+        .then(author => res.status(201).json({
+            _id: author.id,
+            name: `${author.firstName} ${author.lastName}`,
+            userName: author.userName
+          }))
+        .catch(err => {
+          console.error(err);
+          res.status(500).json({ error: 'Something went wrong' });
+        });
+    }
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({ error: 'something went horribly awry' });
+  });
+});
+
+
 app.get("/posts", (req, res) => {
   BlogPost
     .find()
