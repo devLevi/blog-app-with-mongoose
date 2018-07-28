be42674663d85ea4736326ac08da9d54763bf039
Levi Bernard <levichristianbernard@gmail.com>
2018-07-28 11:48:38 -0700

    Added eslint file and modified formating

--- models.js
+++ models.js
@@ -1,51 +1,51 @@
-'use strict';
-
 const mongoose = require('mongoose');
+
 mongoose.Promise = global.Promise;
 
 const authorSchema = mongoose.Schema({
-  firstName: 'string',
-  lastName: 'string',
-  userName: {
-    type: 'string',
-    unique: true
-  }
+    firstName: 'string',
+    lastName: 'string',
+    userName: {
+        type: 'string',
+        unique: true
+    }
 });
 
 const commentSchema = mongoose.Schema({ content: 'string' });
 
 const blogPostSchema = mongoose.Schema({
-  title: 'string',
-  content: 'string',
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'Author' },
-  comments: [commentSchema]
+    title: 'string',
+    content: 'string',
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'Author' },
+    comments: [commentSchema]
 });
 
 blogPostSchema.pre('find', function(next) {
-  this.populate('author');
-  next();
+    this.populate('author');
+    next();
 });
 
 blogPostSchema.pre('findOne', function(next) {
-  this.populate('author');
-  next();
+    this.populate('author');
+    next();
 });
 
 blogPostSchema.virtual('authorName').get(function() {
-  return `${this.author.firstName} ${this.author.lastName}`.trim();
+    return `${this.author.firstName} ${this.author.lastName}`.trim();
 });
 
 blogPostSchema.methods.serialize = function() {
-  return {
-    id: this._id,
-    author: this.authorName,
-    content: this.content,
-    title: this.title,
-    comments: this.comments
-  };
+    return {
+        id: this._id,
+        author: this.authorName,
+        content: this.content,
+        title: this.title,
+        created: this.created,
+        comments: this.comments
+    };
 };
 
-let Author = mongoose.model('Author', authorSchema);
+const Author = mongoose.model('Author', authorSchema);
 const BlogPost = mongoose.model('BlogPost', blogPostSchema);
 
-module.exports = {Author, BlogPost};
+module.exports = { Author, BlogPost };
